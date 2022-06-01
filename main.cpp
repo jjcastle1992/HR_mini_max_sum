bacb5ff072b4c028271cbf981625d6ccf52a162a
James <jjcastle@utexas.edu>
2022-05-31 20:05:59 -0500

    First pass at miniMaxSum function and removed duplicate code (Beyond line 96)

--- main.cpp
+++ main.cpp
@@ -20,12 +20,17 @@ void miniMaxSum(vector<int> arr) {
 
 
     //Need to sort the vector first
-
+    std::sort(arr.begin(), arr.end());
     //if want min, then just sum from beginning of the vector to n-1
-
+    for (int i = 0; i < (arr.size() - 1); i++) {
+        minSum += arr[i];
+    }
     //if want max, then sum from beginning + 1 of the vector to n
-
+    for (int i = 1; i < arr.size(); i++) {
+        maxSum += arr[i];
+    }
     //Must print the min value and then the max value
+    cout << minSum << " " << maxSum;
 
 }
 
@@ -43,83 +48,6 @@ int main()
         int arr_item = stoi(arr_temp[i]);
 
         //Data validate that arr_item is at least 1 and not greater than 1e9.
-        arr[i] = arr_item;
-    }
-
-    miniMaxSum(arr);
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-            s.begin(),
-            find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-            find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-            s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
-}
-#include <bits/stdc++.h>
-
-using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
-
-/*
- * Complete the 'miniMaxSum' function below.
- *
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
-
-void miniMaxSum(vector<int> arr) {
-
-}
-
-int main()
-{
-
-    string arr_temp_temp;
-    getline(cin, arr_temp_temp);
-
-    vector<string> arr_temp = split(rtrim(arr_temp_temp));
-
-    vector<int> arr(5);
-
-    for (int i = 0; i < 5; i++) {
-        int arr_item = stoi(arr_temp[i]);
-
         arr[i] = arr_item;
     }
 
